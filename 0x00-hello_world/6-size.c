b268a1b8b5f924ddfaef7b9376d667ace320cdf1
islam solaiman <islam.solaiman92@gmail.com>
2023-03-16 07:54:26 -0700

    Task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\n
+			Size of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)
+			\nSize of a float: 4 byte(s)");
+}
