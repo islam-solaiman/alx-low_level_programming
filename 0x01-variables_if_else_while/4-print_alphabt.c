b6e5233621356f3ab486a845ada6bb2cf60076ed
islam solaiman <islam.solaiman92@gmail.com>
2023-03-17 14:37:54 -0700

    Task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alphabet;
+
+	alphabet = 97;
+	while ((alphabet >= 97) && (alphabet <= 122))
+	{
+		if ((alphabet != 113) && (alphabet != 101))
+			putchar(alphabet);
+		alphabet++;
+	}
+	putchar(10);
+	return (0);
+}
