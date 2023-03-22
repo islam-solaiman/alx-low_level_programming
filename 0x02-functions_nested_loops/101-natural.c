d5eb8f136be6c44ba1d2ebbf19c492cf2e79f930
islam solaiman <islam.solaiman92@gmail.com>
2023-03-22 04:54:26 -0700

    Task13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int x;
+
+	n = 0;
+	x = n;
+	while (n < 1024)
+	{
+		if (((n % 3) == 0) || ((n % 5) == 0) || ((n % 6) == 0))
+			x += n;
+		n++;
+	}
+	printf("%d\n", x);
+}
