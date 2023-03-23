f9f71a62f0c4ee35b700dfa29223325a3f2e8377
islam solaiman <islam.solaiman92@gmail.com>
2023-03-23 11:25:07 -0700

    Task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,20 +15,19 @@ int main(void)
 	int i;
 
 	i = 1;
-	printf("%d", i);
-	i++;
-	while (i <= 100)
+	while (i < 100)
 	{
 		if ((i % 3 == 0) && (i % 5) == 0)
-			printf(" FizzBuzz");
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
-			printf(" Fizz");
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf(" Buzz");
+			printf("Buzz ");
 		else
-			printf(" %d", i);
+			printf("%d ", i);
 		i++;
 	}
-	printf("\n");
+	printf("Buzz\n");
+
 	return (0);
 }
