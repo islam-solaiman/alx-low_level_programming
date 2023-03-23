fd612ceedc31698ee65a72eed4ca4a51c8cdd4de
islam solaiman <islam.solaiman92@gmail.com>
2023-03-23 15:19:41 -0700

    Task10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - function that prints a triangle, followed by a new line.
+ * @size: is the size of the triangle.
+ */
+
+void print_triangle(int size)
+{
+	int raw;
+
+	raw = 1;
+	while (raw <= size)
+	{
+		int hash;
+		int space;
+
+		hash = 1;
+		space = size - raw;
+		while (space >= hash)
+		{
+			printf(" ");
+			space--;
+		}
+		while (hash <= raw)
+		{
+			printf("#");
+			hash++;
+		}
+		raw++;
+		printf("\n");
+	}
+	/*printf("\n");*/
+}
