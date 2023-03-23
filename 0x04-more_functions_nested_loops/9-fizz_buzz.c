097332d655ef4197db029a8517d08879b41228b7
islam solaiman <islam.solaiman92@gmail.com>
2023-03-23 10:06:49 -0700

    Task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ * Resturn: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i < 100)
+		{
+			if (((i % 3) == 0) && ((i % 5) == 0))
+				printf("FizzBuzz ");
+			else if ((i % 3) == 0)
+			       printf("Fizz ");
+			else if ((i % 5) == 0)
+				printf("Buzz ");
+			else
+				printf("%d ", i);	
+		}
+		if (i == 100)
+			printf("Buzz\n");
+		i++;
+	
+	}
+	return (0);
+}
