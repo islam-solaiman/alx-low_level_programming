1d95adf586a78e2467101e6cfd7df8c6f371b62f
islam solaiman <islam.solaiman92@gmail.com>
2023-03-28 03:20:36 -0700

    Task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,19 +10,24 @@ int _atoi(char *s)
 {
 	int i;
 	int d;
+	int x;
 	int temp;
 
 	i = 0;
 	d = 1;
+	x = 0;
 	temp = 0;
-	while (!((s[i] >= '0' && s[i] <= '9')))
-		i++;
-	while (s[i] == '-' || s[i] == '+')
+	while (s[i] && (!((s[i] >= '0' && s[i] <= '9'))))
 	{
+		if (s[i] == '-')
+			x++;
 		i++;
 	}
-	if (s[i - 1] == '-')
+	if (x % 2 > 0)
 		d *= -1;
+	i = 0;
+	while (!((s[i] >= '0' && s[i] <= '9')))
+		i++;
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		temp = (s[i] - '0') + (temp * 10);
