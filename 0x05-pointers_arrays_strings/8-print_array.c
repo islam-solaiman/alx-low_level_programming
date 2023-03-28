243b30d210d831ae8858c292eb3ad05f085e0955
islam solaiman <islam.solaiman92@gmail.com>
2023-03-27 18:29:11 -0700

    Task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,15 @@ void print_array(int *a, int n)
 	int i;
 
 	i = 0;
-	while (i < (n - 1))
+	if (n == 1)
+		printf("%d\n", i);
+	else
 	{
-		printf("%d, ", a[i]);
-		i++;
+		while (i < (n - 1))
+		{
+			printf("%d, ", a[i]);
+			i++;
+		}
+		printf("%d\n", a[i]);
 	}
-	printf("%d\n", a[i]);
 }
