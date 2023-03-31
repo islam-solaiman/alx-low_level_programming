52e29535529264793ee8a6998747bc19cf55738a
islam solaiman <islam.solaiman92@gmail.com>
2023-03-30 18:04:50 -0700

    Task9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,12 +7,18 @@
 
 void print_number(int n)
 {
+	unsigned int n1;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-	if (n / 10 > 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+
+	n1 = n;
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar(n1 % 10 + '0');
 }
