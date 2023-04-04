cf0ab03114183eed2a3331ceffdbed40554bb0f5
islam solaiman <islam.solaiman92@gmail.com>
2023-04-04 16:44:59 -0700

    Task8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,12 +13,13 @@ void print_diagsums(int *a, int size)
 	int i;
 	unsigned int res1, res2;
 
+	i = 0;
 	res1 = 0;
 	res2 = 0;
 	while (i < size)
 	{
-		res1 += a[(size * 0) + i];
-		res2 += a[(size * (i + 1) - (i + 1))];
+		res1 += a[(size * i) + i];
+		res2 += a[(size * (i + 1)) - (i + 1)];
 		i++;
 	}
 	printf("%d, %d\n", res1, res2);
