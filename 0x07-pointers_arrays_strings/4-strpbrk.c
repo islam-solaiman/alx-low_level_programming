83a70af8a86a302751ca994647e1b90c34330ea7
islam solaiman <islam.solaiman92@gmail.com>
2023-04-04 14:22:46 -0700

    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

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring.
+ * @s: string to be scanned.
+ * @accept: string containing the characters to match.
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int count;
+
+	j = 0;
+	count = 0;
+	if (s[j] == '\0')
+		return (0);
+	while (s[j])
+	{
+		i = 0;
+		while (accept[i])
+		{
+			if (s[j] == accept[i])
+			{
+				count++;
+			}
+			i++;
+		}
+		j++;
+	}
+	if (count == 0)
+		return (0);
+	else
+		return (count - 2);
+}
