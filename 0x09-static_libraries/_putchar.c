7b0dc968b9e7c7a30f6d6c8f1f5abc8e86e57ca7
islam solaiman <islam.solaiman92@gmail.com>
2023-04-07 15:43:45 -0700

    Task0 update

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -2,10 +2,9 @@
 #include <unistd.h>
 
 /**
- * Description : _pustchar.c  function to print single character
- *
- * c is the character to bassed as a parameter to be printed
- *
+ * _putchar -  function to print single character
+ * @c: is the character to bassed as a parameter to be printed
+ * Return: 1 when success and -1 if error
  */
 int _putchar(char c)
 {
