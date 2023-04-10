3cdec3c594a37178304fa574a6477f3625fdf030
islam solaiman <islam.solaiman92@gmail.com>
2023-04-09 17:27:16 -0700

    C - argc, argv - Task5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 {
 	int change, quarter, ten, five, two, one;
 
-	if (argc != 2 || **argv > '9' || **argv < '0')
+	if (argc != 2 || *argv[1] > '9' || *argv[1] < '0')
 	{
 		printf("Error\n");
 		return (1);
