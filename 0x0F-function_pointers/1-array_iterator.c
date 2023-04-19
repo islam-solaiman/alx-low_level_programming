c7d669c58de1e03ea7c30b38ad61f3232e5f4e2f
islam solaiman <islam.solaiman92@gmail.com>
2023-04-19 16:34:24 -0700

    0x0F. C - Function pointers - TASK1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
@@ -9,7 +10,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
 	i = 0;
 	if (!array || !action)
