d14eae775fe81c97738edc2a86b86af4bf2bbddb
islam.solaiman <islam.solaiman92@gmail.com>
2023-05-02 16:47:30 -0700

    0x13. C - More singly linked lists - TASK9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp = tmp->next;
 		i++;
 	}
-	return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
