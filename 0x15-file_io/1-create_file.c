5a7af7af5582f3e03e7a8886ce2975f27705dc35
islam.solaiman <islam.solaiman92@gmail.com>
2023-05-08 16:14:49 -0700

    0x15. C - File I/O - TASK1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,9 @@ int create_file(const char *filename, char *text_content)
 	int write_bytes;
 
 	i = 0;
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	while (text_content[i])
+	while (text_content && text_content[i])
 		i++;
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	write_bytes = write(fd, text_content, i);
