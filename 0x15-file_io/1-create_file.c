8e72d62cdf33eca48b9a9bab94bd9af0344aa581
islam.solaiman <islam.solaiman92@gmail.com>
2023-05-08 16:10:50 -0700

    0x15. C - File I/O - TASK1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file -  function that creates a file.
+ * @filename: POINTER TO A FILe
+ * @text_content: string to write to the file
+ * Return: truncated file
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+	int write_bytes;
+
+	i = 0;
+	if (!filename)
+		return (-1);
+	while (text_content[i])
+		i++;
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write_bytes = write(fd, text_content, i);
+
+	if (fd == -1 || write_bytes == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+}
